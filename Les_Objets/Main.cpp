29f9ae42cd6de836bdaf5ffa2aa9a18eca99f68b
Crims0nnn <m.brahim@ludus-academie.com>
2022-05-05 10:07:38 +0200

    Core programme cpp V1

--- Les_Objets/Main.cpp
+++ Les_Objets/Main.cpp
@@ -1,10 +1,13 @@
 //Fichier main.cpp
-//
+//Version 1.0
+//Branche release
 #include <stdio.h>
 #include <string>
 #include <string.h>
 
 int main() {
+	
+	printf_s("Version 1.0 du prograùùe Cpp\n");
 
 	return 0;
 }
